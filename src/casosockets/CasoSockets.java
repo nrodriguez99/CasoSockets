2a239f59e9a805fb9608d33300c4669c33dd720e
Natalia Rodriguez Arias <naty_rodriguez_arias@hotmail.com>
2019-10-10 00:14:56 -0600

    Commit inicial

---
+++ src/casosockets/CasoSockets.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casosockets;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author naty9
+ */
+public class CasoSockets {
+
+    public static void main(String[] args) throws Exception {
+        try{
+            ServerSocket server=new ServerSocket(8888);
+            int counter=0;
+            System.out.println("Server Started ....");
+            while(true){
+                counter++;
+                Socket serverClient=server.accept();  //server accept the client connection request
+                System.out.println(" >> " + "Client No:" + counter + " started!");
+                Servidor sct = new Servidor(serverClient,counter); //send  the request to a separate thread
+                sct.start();
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+}
---
+++ src/casosockets/Servidor.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casosockets;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ *
+ * @author naty9
+ */
+public class Servidor extends Thread {
+    
+    Socket serverClient;
+    int clientNo;
+    int squre;
+  
+    Servidor(Socket inSocket,int counter){
+        serverClient = inSocket;
+        clientNo=counter;
+    }
+    public void run(){
+        try{
+            DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
+            DataOutputStream outStream = new DataOutputStream(serverClient.getOutputStream());
+            String clientMessage="", serverMessage="";
+        while(!clientMessage.equals("bye")){
+            clientMessage=inStream.readUTF();
+            System.out.println("From Client-" +clientNo+ ": Number is :"+clientMessage);
+            squre = Integer.parseInt(clientMessage) * Integer.parseInt(clientMessage);
+            serverMessage="From Server to Client-" + clientNo + " Square of " + clientMessage + " is " +squre;
+            outStream.writeUTF(serverMessage);
+            outStream.flush();
+        }
+        inStream.close();
+        outStream.close();
+        serverClient.close();
+        }catch(Exception ex){
+            System.out.println(ex);
+        }finally{
+            System.out.println("Client -" + clientNo + " exit!! ");
+        }
+    }
+}
